fff9608ce340bf175f535f4fa0016904555e9247
SantiQPS <santiago.quintero@correounivalle.edu.co>
2018-03-21 18:13:38 -0500

    Add files via upload
    El programa muestra el numero de procesos del computador, la memoria ram libre, y el porcentaje de HD libre

---
+++ miscript2.sh
@@ -0,0 +1,26 @@
+
+#l/bin/bash
+#numero de procesos
+NUEMPROCESOS=$( ps ax | tail -n +2 | wc -l)
+
+#memoria ram libre
+valorMemoriaLibre=$(vmstat | tail -n 1 | awk '{print $4}')
+
+#memoria ram total
+valorMemoriaTotal=$(free | grep Mem | awk '{print $2}')
+
+#porcentaje memoria ram libre
+mulRam=$((${valorMemoriaLibre} * 100))
+porcentajeRam=$((${mulRam} / ${valorMemoriaTotal}))
+
+
+echo ${porcentajeRam}
+#porcentaje libre HD
+
+porcentajeHD=$(df | grep root | awk '{print $5}' | cut -c1-2)
+echo ${porcentajeHD}
+
+echo ${NUEMPROCESOS}
+curl --silent --request POST --header "X-THINGSPEAKAPIKEY:DEDTHRLIXWNGPFR3" --data "field1=${NUEMPROCESOS}&field2=${porcentajeRam}&field3=${porcentajeHD}" http://api.thingspeak.com/update
+
+
